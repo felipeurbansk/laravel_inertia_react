749915ae77a35f9a4549836d174cdaf9a7283f24
Felipe Urbanski <felipeurbansk@gmail.com>
2021-08-29 15:29:52 -0300

    finished base template

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <h1>
+            Hello world
+
+            <p>Teste</p>
+        </h1>
+    );
+};
+
+export default Home;
